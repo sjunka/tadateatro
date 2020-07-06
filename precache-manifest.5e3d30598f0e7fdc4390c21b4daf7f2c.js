30d170d5e1b1d3fd6839d5c9ad4372886a06d1ee
Sergio Junca <sjunca@phase2technology.com>
2020-07-06 15:12:45 -0500

    Updates

---
+++ precache-manifest.5e3d30598f0e7fdc4390c21b4daf7f2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c180c81ca5a240a8c01906ff469f51d",
+    "url": "/tadateatro/index.html"
+  },
+  {
+    "revision": "35784ee8e0c7395e2889",
+    "url": "/tadateatro/static/js/2.cacb5de9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tadateatro/static/js/2.cacb5de9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600a0293a4b607d2cf53",
+    "url": "/tadateatro/static/js/main.ceb0b3ef.chunk.js"
+  },
+  {
+    "revision": "ddbc66c72dba886a3133",
+    "url": "/tadateatro/static/js/runtime-main.fb987f6f.js"
+  }
+]);
