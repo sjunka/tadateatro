92b5e14d999e299f5f810d914c253f885db5c953
Sergio Junca <sjunca@phase2technology.com>
2020-07-06 17:38:16 -0500

    fix cards2

--- src/ProTip.js
+++ src/ProTip.js
@@ -94,42 +94,13 @@ const tiers = [
     buttonVariant: 'outlined',
   },
 ];
-const footers = [
-  {
-    title: 'Company',
-    description: ['Team', 'History', 'Contact us', 'Locations'],
-  },
-  {
-    title: 'Features',
-    description: [
-      'Cool stuff',
-      'Random feature',
-      'Team feature',
-      'Developer stuff',
-      'Another one',
-    ],
-  },
-  {
-    title: 'Resources',
-    description: [
-      'Resource',
-      'Resource name',
-      'Another resource',
-      'Final resource',
-    ],
-  },
-  {
-    title: 'Legal',
-    description: ['Privacy policy', 'Terms of use'],
-  },
-];
 
 export default function Pricing() {
   const classes = useStyles();
 
   return (
     <div>
-      <CssBaseline />
+      {/* <CssBaseline /> */}
 
       {/* Hero unit */}
       <Container maxWidth="sm" component="main" className={classes.heroContent}>
