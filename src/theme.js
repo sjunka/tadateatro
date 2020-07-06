283ecc909dfe49147881bd5a24fa7a6c6668c40c
Sergio Junca <sjunca@phase2technology.com>
2020-07-06 15:20:07 -0500

    colors

--- src/theme.js
+++ src/theme.js
@@ -5,10 +5,16 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
     },
     secondary: {
-      main: '#19857b',
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
     },
     error: {
       main: red.A400,
