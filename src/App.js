620ccd5fb640a2b225d6c4de117821b0d279ff82
Sergio Junca <sjunca@phase2technology.com>
2020-07-06 15:09:22 -0500

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import ProTip from './ProTip';
+import SignUp from './SignUp';
+import MainFeaturedPost from './MainFeaturedPost';
+import Drawer from './Drawer';
 
-function App() {
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+  const [openDrawer, SetOpenDrawer] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+              onClick={() => SetOpenDrawer((preOpenDrawer) => true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Tada Teatro
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+
+        <Drawer
+          open={openDrawer}
+          close={() => SetOpenDrawer((preOpenDrawer) => false)}
+        ></Drawer>
+      </Box>
+
+      <MainFeaturedPost />
+      <ProTip />
+
+      <SignUp />
+    </Container>
   );
 }
-
-export default App;
---
+++ src/MainFeaturedPost.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles((theme) => ({
+  mainFeaturedPost: {
+    position: 'relative',
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    height: '300 px',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,.3)',
+  },
+  mainFeaturedPostContent: {
+    height: '300 px',
+    position: 'relative',
+    padding: theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      padding: theme.spacing(6),
+      paddingRight: 0,
+    },
+  },
+}));
+
+export default function MainFeaturedPost(props) {
+  const classes = useStyles();
+  const { post } = props;
+
+  return (
+    <Paper
+      className={classes.mainFeaturedPost}
+      //   style={{ backgroundImage: `url(${post.image})` }}
+      style={{
+        backgroundImage: classes.backgroundImage,
+        height: '300 px',
+      }}
+    >
+      {/* Increase the priority of the hero background image */}
+      {
+        <img
+          style={{ display: 'none', height: '300 px' }}
+          src={classes.backgroundImage}
+          alt="no image"
+        />
+      }
+      <div className={classes.overlay} />
+      <Grid container>
+        <Grid item md={6}>
+          <div className={classes.mainFeaturedPostContent}>
+            <Typography
+              component="h1"
+              variant="h3"
+              color="inherit"
+              gutterBottom
+            >
+              Title of a longer featured blog post
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              Multiple lines of text that form the lede, informing new readers
+              quickly and efficiently about what's most interesting in this
+              post's contents.
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
+
+MainFeaturedPost.propTypes = {
+  post: PropTypes.object,
+};
